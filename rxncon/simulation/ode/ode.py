e6da94cc09f755f72a7d2659a331d0f8191bf9cc
Jesper Romers <jesper.romers@hu-berlin.de>
2016-02-01 11:25:46 +0100

    before push

--- rxncon/simulation/ode/ode.py
+++ rxncon/simulation/ode/ode.py
@@ -4,7 +4,12 @@ import rxncon.simulation.ode.polynomials as pol
 
 
 class ODE:
+    @tc.typecheck
     def __init__(self, time_derivative: pol.Symbol, polynomial: pol.Polynomial):
         self.time_derivative = time_derivative
         self.polynomial = polynomial
+
+    @property
+    def symbols(self):
+        return self.polynomial.symbols.union({self.time_derivative})
 
