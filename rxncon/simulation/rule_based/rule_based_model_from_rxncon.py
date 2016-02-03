fce43747f3ef09ab7ff341862d5358f896951d56
Basti <devnull@localhost>
2016-02-03 19:08:13 +0100

    changed some stuff. Now the definitions default dict gets it's values updated outside.

--- rxncon/simulation/rule_based/rule_based_model_from_rxncon.py
+++ rxncon/simulation/rule_based/rule_based_model_from_rxncon.py
@@ -7,8 +7,8 @@ import typecheck as tc
 import rxncon.core.rxncon_system as rxs
 import rxncon.core.state as sta
 import rxncon.simulation.rule_based.rule_based_model as rbm
-from rxncon.set_semantics.set_semantics import determine_base_rule_conditions, expand_base_rule_condition, \
-    disjunct_rule_conditions_from_rule_conditions, rule_from_rule_condition_and_molecule_definitions
+import rxncon.semantics.state_flow as flo
+import rxncon.venntastic.sets as venn
 
 
 class Default(Enum):
@@ -17,32 +17,74 @@ class Default(Enum):
     modification_prefix = 'Mod'
 
 
-@tc.typecheck
+#@tc.typecheck
 def rule_based_model_from_rxncon(rxnconsys: rxs.RxnConSystem) -> rbm.RuleBasedModel:
     molecule_defs = molecule_defs_from_rxncon(rxnconsys)
 
-    rule_conditions = []
+    flows = []
 
     for reaction in rxnconsys.reactions:
-        base_rule_conditions = determine_base_rule_conditions(rxnconsys.strict_contingencies_for_reaction(reaction),
-                                                              rxnconsys.source_contingencies_for_reaction(reaction))
+        boolean_flow = flo.boolean_state_flows(reaction, rxnconsys.strict_contingencies_for_reaction(reaction),
+                                                       rxnconsys.source_contingencies_for_reaction(reaction))
 
         derived_rule_conditions = []
 
-        for condition in base_rule_conditions:
-            derived_rule_conditions += \
-                expand_base_rule_condition(condition,
-                                           rxnconsys.quantitative_contingencies_for_reaction(reaction))
-
-        rule_conditions += derived_rule_conditions
-
+        # for condition in base_rule_conditions:
+        #     derived_rule_conditions += \
+        #         expand_base_rule_condition(condition,
+        #                                    rxnconsys.quantitative_contingencies_for_reaction(reaction))
+        #
+        # rule_conditions += derived_rule_conditions
+        flows += boolean_flow
+    # rules = []
+    #
+    # for disjunct_rule_condition in disjunct_rule_conditions_from_rule_conditions(rule_conditions):
+    #     rules.append(rule_from_rule_condition_and_molecule_definitions(disjunct_rule_condition, molecule_defs))
+    #
+    # return rules
     rules = []
-
-    for disjunct_rule_condition in disjunct_rule_conditions_from_rule_conditions(rule_conditions):
-        rules.append(rule_from_rule_condition_and_molecule_definitions(disjunct_rule_condition, molecule_defs))
+    for flow in flows:
+        rules.append(rule_from_flow_and_molecule_definitions(flow, molecule_defs))
 
     return rules
 
+#@tc.typecheck
+def rule_from_flow_and_molecule_definitions(flow: flo.StateFlow, molecule_defs: List[rbm.MoleculeDefinition]) -> rbm.Rule:
+    name_to_association_specifications = defaultdict(set)
+    name_to_modification_specifications = defaultdict(set)
+    name_to_valid_compartments = defaultdict(set)
+
+    assert len(flow.source.to_nested_list_form()) == 1
+
+    for source in flow.source.to_nested_list_form()[0]:
+        if isinstance(source.expr.value, sta.InterProteinInteractionState):
+            name_to_association_specifications = association_specification_from_set(name_to_association_specifications, source)
+
+    for target in flow.target.to_nested_list_form()[0]:
+        if isinstance(source.expr.value, sta.InterProteinInteractionState):
+            name_to_association_specifications = association_specification_from_set(name_to_association_specifications, source)
+
+
+# modification_specs = [rbm.ModificationSpecification(modification_defs[0], 'P')]
+# association_specs  = [rbm.AssociationSpecification(association_defs[0], rbm.OccupationStatus.not_occupied)]
+# localization_spec  = rbm.LocalizationSpecification(localization_def, 'Cytoplasm')
+
+def association_specification_from_set(name_to_association_specifications: defaultdict, set_state: venn.Set):
+    name_to_association_definition = defaultdict(set)
+    if isinstance(set_state, venn.Complement):
+        assert isinstance(set_state.expr, venn.PropertySet)
+        assert isinstance(set_state.expr.value, sta.State)
+        name_to_association_definition = name_to_assoc_defs_from_state(name_to_association_definition, set_state.expr.value)
+        first_comp_assoc_spec = rbm.AssociationSpecification(list(name_to_association_definition[set_state.expr.value.first_component.name])[0], rbm.OccupationStatus.not_occupied)
+        second_comp_assoc_spec =rbm.AssociationSpecification(name_to_association_definition[set_state.expr.value.second_component.name], rbm.OccupationStatus.not_occupied)
+        name_to_association_specifications[set_state.expr.value.first_component.name].add(first_comp_assoc_spec)
+        name_to_association_specifications[set_state.expr.value.second_component.name].add(second_comp_assoc_spec)
+    elif isinstance(set_state, venn.PropertySet):
+        assert isinstance(set_state.value, sta.State)
+        pass
+    else:
+        raise NotImplementedError
+    return name_to_association_specifications
 
 @tc.typecheck
 def molecule_defs_from_rxncon(rxnconsys: rxs.RxnConSystem) -> List[rbm.MoleculeDefinition]:
@@ -59,9 +101,9 @@ def molecule_defs_from_rxncon(rxnconsys: rxs.RxnConSystem) -> List[rbm.MoleculeD
 
     return molecule_definitions
 
-
 @tc.typecheck
-def name_to_mod_defs_from_state_or_reaction(name_to_modification_definitions: dict, state: sta.CovalentModificationState, reaction: rxs.rxn.Reaction) -> dict:
+def name_to_mod_defs_from_state_or_reaction(state: sta.CovalentModificationState, reaction: rxs.rxn.Reaction) -> dict:
+    name_to_modification_definitions = defaultdict(set)
     modifiers = [Default.unmodified.value, state.modifier.value]
     domain = state.substrate.domain if state.substrate.domain else '{0}{1}'.format(Default.modification_prefix.value, reaction.subject.name)
 
@@ -71,8 +113,8 @@ def name_to_mod_defs_from_state_or_reaction(name_to_modification_definitions: di
 
 
 @tc.typecheck
-def name_to_assoc_defs_from_state(name_to_association_definitions: dict, state: tc.any(sta.InterProteinInteractionState, sta.IntraProteinInteractionState)):
-
+def name_to_assoc_defs_from_state(state: tc.any(sta.InterProteinInteractionState, sta.IntraProteinInteractionState)):
+    name_to_association_definitions = defaultdict(set)
     first_domain = state.first_component.domain if state.first_component.domain \
         else '{0}{1}'.format(Default.association_prefix.value, state.second_component.name)
 
@@ -103,13 +145,16 @@ def name_to_defs_from_rxncon(rxnconsys: rxs.RxnConSystem):
         for state in [x for x in (reaction.source, reaction.product) if x]:
             if isinstance(state, sta.CovalentModificationState):
                 names = name_to_names_from_reaction(names, reaction.subject.name, state.substrate.name)
-                name_to_modification_definitions = name_to_mod_defs_from_state_or_reaction(name_to_modification_definitions,
-                                                                                           state, reaction)
+                name_to_modification_definitions.update({key: (name_to_modification_definitions[key].union(value)
+                                                       if key in name_to_modification_definitions else value)
+                                                       for key, value in name_to_mod_defs_from_state_or_reaction(state, reaction).items()})
 
             elif isinstance(state, sta.InterProteinInteractionState) or isinstance(state, sta.IntraProteinInteractionState):
                 names = name_to_names_from_reaction(names, state.first_component.name, state.second_component.name)
-                name_to_association_definitions = name_to_assoc_defs_from_state(name_to_association_definitions,
-                                                                                state)
+
+                name_to_association_definitions.update({key: (name_to_association_definitions[key].union(value)
+                                                       if key in name_to_association_definitions else value)
+                                                       for key, value in name_to_assoc_defs_from_state(state).items()})
 
             elif isinstance(state, sta.SynthesisDegradationState):
                 names = name_to_names_from_reaction(names, reaction.subject.name, state.component.name)
@@ -118,18 +163,3 @@ def name_to_defs_from_rxncon(rxnconsys: rxs.RxnConSystem):
                 names = name_to_names_from_reaction(names, reaction.subject.name, state.substrate.name)
                 name_to_valid_compartments[state.substrate.name].add(state.compartment)
     return names, name_to_modification_definitions, name_to_association_definitions, name_to_valid_compartments
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
