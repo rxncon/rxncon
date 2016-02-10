ade95311eaa9cd9ef405d85a0c4f43f04baedd7d
Jesper Romers <jesper.romers@hu-berlin.de>
2016-02-10 16:38:59 +0100

    exec eval ftw

--- rxncon/simulation/ode/ode.py
+++ rxncon/simulation/ode/ode.py
@@ -6,8 +6,6 @@ import rxncon.simulation.ode.polynomials as pol
 
 
 class ODE:
-    prefix = 'd_t_'
-
     @tc.typecheck
     def __init__(self, time_derivative: pol.Symbol, polynomial: pol.Polynomial):
         self.time_derivative = time_derivative
@@ -18,16 +16,52 @@ class ODE:
         return self.polynomial.symbols.union({self.time_derivative})
 
     def to_py_code(self):
-        lhs = self.prefix + self.time_derivative.name
         rhs = ' + '.join(_polynomial_term_to_py_code(x) for x in self.polynomial.terms)
 
-        return '{0} = {1}'.format(lhs, rhs)
+        return '{0}'.format(rhs)
 
 
 class ODESystem:
+    name_func = 'f'
+    name_y = 'ys'
+    name_t = 't'
+
     def __init__(self, odes: List[ODE]):
         self.odes = odes
         self._validate()
+
+    @property
+    def odeint_function(self):
+        indent = '    '
+        body_statements = self.to_py_code_symbol_defs() + self.to_py_code_function_defs() + [self.to_py_code_return_statement()]
+        body_string = indent + indent.join(body_statements)
+
+        exec('def {0}({1}, {2}):\n{3}'.format(self.name_func, self.name_y, self.name_t, body_string))
+
+        return eval(self.name_func)
+
+    def to_py_code_symbol_defs(self):
+        symbol_defs = []
+
+        for i, ode in enumerate(self.odes):
+            symbol_defs.append('{0} = {1}[{2}]\n'.format(ode.time_derivative.name, self.name_y, i))
+
+        return symbol_defs
+
+    def to_py_code_function_defs(self):
+        function_defs = []
+
+        for i, ode in enumerate(self.odes):
+            function_defs.append('{0}{1} = {2}\n'.format(self.name_func, i, ode.to_py_code()))
+
+        return function_defs
+
+    def to_py_code_return_statement(self):
+        func_names = ['{0}{1}'.format(self.name_func, i) for i, ode in enumerate(self.odes)]
+        func_string = ', '.join(func_names)
+
+        return 'return [{0}]'.format(func_string)
+
 
     def _validate(self):
         if not len(set(x.time_derivative for x in self.odes)) == len(self.odes):
--- rxncon/test/simulation/ode/test_odes.py
+++ rxncon/test/simulation/ode/test_odes.py
@@ -1,4 +1,5 @@
 import pytest
+import math
 
 import rxncon.simulation.ode.polynomials as pol
 import rxncon.simulation.ode.ode as ode
@@ -16,12 +17,46 @@ def test_doubly_defined_time_derivatives(first_lotke_volterra_ode, second_lotke_
         ode_sys = ode.ODESystem([first_lotke_volterra_ode, second_lotke_volterra_ode])
 
 
-def test_single_ode_to_py_code(first_lotke_volterra_ode, second_lotke_volterra_ode):
-    # The order of the terms in each ODE is immaterial, so there are two possible code realizations for the ODEs.
-    assert first_lotke_volterra_ode.to_py_code() == 'd_t_x = (-1) * (y ** 1 * x ** 1) + (1) * (x ** 1)' or \
-        first_lotke_volterra_ode.to_py_code() == 'd_t_x = (1) * (x ** 1) + (-1) * (y ** 1 * x ** 1)'
-    assert second_lotke_volterra_ode.to_py_code() == 'd_t_y = (1) * (y ** 1 * x ** 1) + (-1) * (y ** 1)' or \
-        second_lotke_volterra_ode.to_py_code() == 'd_t_y = (-1) * (y ** 1) + (1) * (y ** 1 * x ** 1)'
+def test_single_ode_to_py_code(first_lotke_volterra_ode, second_lotke_volterra_ode, first_ode_codes, second_ode_codes):
+    # Due to commutativity there are four possible code realizations for each ODE.
+    assert first_lotke_volterra_ode.time_derivative.name == 'x'
+    assert first_lotke_volterra_ode.to_py_code() in first_ode_codes
+
+    assert second_lotke_volterra_ode.time_derivative.name == 'y'
+    assert second_lotke_volterra_ode.to_py_code() in second_ode_codes
+
+
+def test_ode_system_symbol_definition(lotke_volterra_system):
+    assert lotke_volterra_system.to_py_code_symbol_defs() == ['x = ys[0]\n', 'y = ys[1]\n']
+
+
+def test_ode_system_function_definition(lotke_volterra_system):
+    assert lotke_volterra_system.to_py_code_return_statement() == 'return [f0, f1]'
+
+
+def test_ode_odeint_function(lotke_volterra_system):
+    f = lotke_volterra_system.odeint_function
+
+    x_y_to_expected_f_x_f_y = [
+        ([0.2, 0.3], [0.14, -0.24]),
+        ([0.0, 0.0], [0.0, 0.0]),
+        ([0.1, 0.1], [0.09, -0.09]),
+        ([0.5, 1.0], [0.0, -0.5]),
+        ([1.0, 0.5], [0.5, 0.0]),
+        ([2.0, 0.1], [1.8, 0.1]),
+        ([-3.0, 2.0], [3.0, -8.0]),
+        ([0.3, -0.8], [0.54, 0.56])
+    ]
+
+    # Time independent, so t == t0
+    t0 = 0.0
+    for xs, fs in x_y_to_expected_f_x_f_y:
+        assert math.isclose(f(xs, t0)[0], fs[0]) and math.isclose(f(xs, t0)[1], fs[1])
+
+
+@pytest.fixture
+def lotke_volterra_system(first_lotke_volterra_ode, second_lotke_volterra_ode):
+    return ode.ODESystem([first_lotke_volterra_ode, second_lotke_volterra_ode])
 
 @pytest.fixture
 def first_lotke_volterra_ode():
@@ -43,3 +78,15 @@ def second_lotke_volterra_ode():
                           pol.PolynomialTerm(pol.Monomial({pol.MonomialFactor(x, 1), pol.MonomialFactor(y, 1)}), 1)})
 
     return ode.ODE(y, rhs)
+
+
+@pytest.fixture
+def first_ode_codes():
+    return ['(-1) * (y ** 1 * x ** 1) + (1) * (x ** 1)',  '(1) * (x ** 1) + (-1) * (y ** 1 * x ** 1)',
+        '(-1) * (x ** 1 * y ** 1) + (1) * (x ** 1)', '(1) * (x ** 1) + (-1) * (x ** 1 * y ** 1)']
+
+
+@pytest.fixture
+def second_ode_codes():
+    return ['(1) * (y ** 1 * x ** 1) + (-1) * (y ** 1)', '(1) * (x ** 1 * y ** 1) + (-1) * (y ** 1)',
+        '(-1) * (y ** 1) + (1) * (y ** 1 * x ** 1)', '(-1) * (y ** 1) + (1) * (x ** 1 * y ** 1)']
