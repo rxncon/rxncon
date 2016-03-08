672be6ce25c2523f2a67a74e8068550c8e841cf2
wajnberg <devnull@localhost>
2016-03-08 16:01:08 +0100

    wrote 3 tests that work

--- rxncon/test/simulation/bBM/test_bipartite_boolean_model.py
+++ rxncon/test/simulation/bBM/test_bipartite_boolean_model.py
@@ -3,8 +3,23 @@ import rxncon.venntastic.sets as venn
 import rxncon.simulation.bBM.bipartite_boolean_model as bbm
 import rxncon.syntax.rxncon_from_string as rfs
 
-def test_Factor():
+def test_factor():
     factor= venn.Intersection(venn.PropertySet(rfs.state_from_string("A--B")),
                               venn.PropertySet(rfs.state_from_string("A-{P}")))
     bbm_factor= bbm.Factor(factor)
-    assert bbm_factor.is_equivalent_to(factor)
+    assert bbm_factor.factor.is_equivalent_to(factor)
+
+def test_target():
+    target = bbm.Target("A")
+    assert target.name=="A"
+
+def test_rule():
+    factor = venn.Intersection(venn.PropertySet(rfs.state_from_string("A--B")),
+                               venn.PropertySet(rfs.state_from_string("A-{P}")))
+    rule = bbm.Rule(bbm.Target("A"), bbm.Factor(factor))
+
+
+    assert rule.target == bbm.Target("A")
+    assert rule.factor.factor.is_equivalent_to(factor)
+    #assert rule.factor==factor
+
