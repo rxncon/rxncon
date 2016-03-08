ca47bb243da88f361b00d9cd8f901e324b3e8cd4
Basti <devnull@localhost>
2016-03-08 17:32:48 +0100

    fixed Node

--- rxncon/simulation/bBM/bipartite_boolean_model.py
+++ rxncon/simulation/bBM/bipartite_boolean_model.py
@@ -1,5 +1,8 @@
-from typing import List
+from typing import List, Union
 import rxncon.venntastic.sets as venn
+import rxncon.core.reaction as rxn
+import rxncon.core.state as sta
+
 
 class Bipartite_Boolean_Model:
     def __init__(self, rules: List["Rule"]):
@@ -9,8 +12,31 @@ class Bipartite_Boolean_Model:
     def _validate(self):
         pass
 
+
+class Node:
+    def __init__(self, value: Union[rxn.Reaction, sta.State]):
+        self.value = value
+
+    def __eq__(self, other: 'Node'):
+        if isinstance(self.value, rxn.Reaction) and isinstance(other.value, rxn.Reaction) and self.value == other.value:
+                return True
+        elif isinstance(self.value, sta.State) and isinstance(other.value, sta.State) and self.value == other.value:
+                return True
+        else:
+            return False
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return str(self.value)
+
+
 class Rule:
-    def __init__(self, target: "Target", factor: "Factor"):
+    def __init__(self, target: Node, factor: "Factor"):
         self.target = target
         self.factor = factor
         self._validate()
@@ -18,25 +44,12 @@ class Rule:
     def _validate(self):
         pass
 
-class Target:
-    def __init__(self, name: str ):
-        self.name=name
-        self._validate()
-
-    def __eq__(self, other):
-        return self.name==other.name
-
-    def _validate(self):
-        pass
 
 class Factor:
     def __init__(self, factor: venn.Set ):
         # venn.set e.g.:Union(Intersection(A--B, A_{P}), Intersection(A--C, C-{P})) --> (A--B & A_{P}) |(A--C & C_{P})
         self.factor = factor
         self._validate()
-
-    def __eq__(self, other):
-        return self.factor.is_equivalent_to(other.factor)
 
     def _validate(self):
         pass
