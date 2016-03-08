b0df1a8873f69d620777998a22d97d728fb24572
Basti <devnull@localhost>
2016-03-08 19:04:45 +0100

    started with renaming reactions and states in a proper way

--- rxncon/simulation/bBM/bBM_boolnet_exporter.py
+++ rxncon/simulation/bBM/bBM_boolnet_exporter.py
@@ -1,7 +1,9 @@
 import rxncon.simulation.bBM.bipartite_boolean_model as bBm
-from typing import List
+from typing import List, Union
+import re
 import rxncon.venntastic.sets as venn
-
+import rxncon.core.reaction as rxn
+import rxncon.core.state as sta
 
 
 class bBm_System:
@@ -25,8 +27,8 @@ class bBm_System:
     def _rule_to_string(self, rule: bBm.Rule):
         return "{0}, {1}".format(self._target_to_string(rule.target), self._factors_to_string(rule.factor))
 
-    def _target_to_string(self, target: bBm.Target):
-        return target.name
+    def _target_to_string(self, target: bBm.Node):
+        return self._generate_name(target.value)
 
     def _factors_to_string(self, factor: bBm.Factor):
         nested_factor_list=factor.to_union_list_form()
@@ -40,7 +42,7 @@ class bBm_System:
         return " | ".join(result)
 
     def _bool_and_to_string(self, bool_and: List[venn.Set]):
-        elements=[element.name for element in bool_and]
+        elements=[self._generate_name(element) for element in bool_and]
 
         if len(elements) == 1:
             return elements[0]
@@ -50,5 +52,66 @@ class bBm_System:
             raise NotImplementedError
 
 
-    def _generate_name(self):
-        self.name = blabla(self.value)
+    def _generate_name(self, property_set: venn.PropertySet):
+        if isinstance(property_set.value, rxn.Reaction):
+            return string_from_reaction(property_set.value)
+
+        elif isinstance(property_set.value, sta.InterProteinInteractionState):
+            return string_from_inter_protein_interaction_state(property_set.value)
+
+        elif isinstance(property_set.value, sta.IntraProteinInteractionState):
+            return string_from_intra_protein_interaction_state(property_set.value)
+
+        elif isinstance(property_set.value, sta.CovalentModificationState):
+            return string_from_covalent_modification_state(property_set.value)
+
+        elif isinstance(property_set.value, sta.TranslocationState):
+            return string_from_translocation_state(property_set.value)
+
+        elif isinstance(property_set.value, sta.SynthesisDegradationState):
+            return string_from_synthesis_degradation_state(property_set.value)
+
+
+
+        #re,sub("\W", "", self.name)  # replace everything not in [a-zA-Z0-9_] with nothing
+
+def string_from_reaction(reaction_node: bBm.Node) -> str:
+    def generate_reaction_verb_name(verb: rxn.Verb):
+        if re.search("-", verb.value):
+            return re.sub("-", "minus", verb.value)
+        elif re.search("\+", verb.value):
+            return re.sub("\+", "plus", verb.value)
+
+    verb = generate_reaction_verb_name(reaction_node.value.verb)
+    return "{0}_{1}_{2}".format(reaction_node.value.subject, generate_reaction_verb_name(reaction_node.value.verb), reaction_node.value.object)
+
+def replace_breakets(value):
+    value = re.sub('[\[{(]',"OpenBracket_", value )
+    value = re.sub('[\]})]', "_CloseBracket", value )
+    return value
+
+def string_from_inter_protein_interaction_state(state) -> str:
+    result =  '{0}__{1}'.format(state.first_component, state.second_component)
+    return replace_breakets(result)
+
+
+def string_from_intra_protein_interaction_state(state) -> str:
+    # A_[m]--[n]
+    result = '{0}__OpenBracket_{1}_CloseBracket'.format(state.first_component, state.second_component.domain)
+    return replace_breakets(result)
+
+def string_from_covalent_modification_state(state) -> str:
+    # A-{P}
+    result = '{0}_{1}'.format(state.substrate, state.modifier.value)
+    return replace_breakets(result)
+
+
+def string_from_translocation_state(state) -> str:
+    # A-{cyto}
+    result = '{0}_{1}'.format(state.substrate, state.compartment.value)
+    return replace_breakets(result)
+
+
+def string_from_synthesis_degradation_state(state) -> str:
+    result = '{}'.format(state.component)
+    return replace_breakets(result)
