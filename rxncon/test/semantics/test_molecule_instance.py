2b9e8771879a53b004824f7b91dbfba6a53c9f55
thieme <sebastian.thieme@hu-berlin.de>
2016-03-21 18:04:01 +0100

    changes in string_from_reaction now a subdomain definition is independent from domain definition. added tests for sorting association property instances

--- rxncon/test/semantics/test_molecule_instance.py
+++ rxncon/test/semantics/test_molecule_instance.py
@@ -6,6 +6,8 @@ import rxncon.semantics.molecule_instance as mins
 import rxncon.semantics.molecule_definition as mdef
 import rxncon.semantics.molecule_instance_from_rxncon as mifr
 import rxncon.semantics.molecule_definition_from_rxncon as mdfr
+import rxncon.semantics.molecule_definition as mdf
+import rxncon.core.specification as spe
 
 
 def test_commutative_diagram_for_complement_operation(mol_def, state_sets):
@@ -39,3 +41,135 @@ def state_sets():
     return [
         venn.PropertySet(rfs.state_from_string('A--B'))
     ]
+
+@pytest.fixture
+def specification_generic_domain_B():
+    return spe.Specification("B", 'generic', None, None)
+
+@pytest.fixture
+def spec_A_dB():
+    return spe.Specification("A", 'dB', None, None)
+
+@pytest.fixture
+def spec_A_dC():
+    return spe.Specification("A", 'dC', None, None)
+
+@pytest.fixture
+def spec_A_sB():
+    return spe.Specification("A", None, 'sB', None)
+
+@pytest.fixture
+def spec_A_sC():
+    return spe.Specification("A", None, 'sC', None)
+
+@pytest.fixture
+def spec_A_rB():
+    return spe.Specification("A", None, None, 'rB')
+
+@pytest.fixture
+def spec_A_rC():
+    return spe.Specification("A", None, None, 'rC')
+
+@pytest.fixture
+def spec_A_dB_sC():
+    return spe.Specification("A", "dB", "sC", None)
+
+@pytest.fixture
+def spec_A_dB_sD():
+    return spe.Specification("A", "dB", "sD", None)
+
+
+@pytest.fixture
+def spec_A_dB_rC():
+    return spe.Specification("A", "dB", None, "rC")
+
+
+@pytest.fixture
+def spec_A_dB_rD():
+    return spe.Specification("A", "dB", None, "sD")
+
+
+@pytest.fixture
+def spec_A_sB_rC():
+    return spe.Specification("A", None, "sB", "rC")
+
+
+@pytest.fixture
+def spec_A_sB_rD():
+    return spe.Specification("A", None, "sB", "rD")
+
+
+def assoc_instance_A(spec_A_1, spec_A_2, specification_generic_domain_B):
+    return {mins.AssociationPropertyInstance(mdf.AssociationPropertyDefinition(spec_A_1, {specification_generic_domain_B}),
+                                             mdf.OccupationStatus.occupied_known_partner,
+                                             specification_generic_domain_B),
+            mins.AssociationPropertyInstance(mdf.AssociationPropertyDefinition(spec_A_2, {specification_generic_domain_B}),
+                                             mdf.OccupationStatus.occupied_known_partner,
+                                             specification_generic_domain_B)}
+
+
+def expected_ordering_assoc_instance(spec_A_1, spec_A_2, specification_generic_domain_B):
+    return [mins.AssociationPropertyInstance(mdf.AssociationPropertyDefinition(spec_A_1, {specification_generic_domain_B}),
+                                             mdf.OccupationStatus.occupied_known_partner,
+                                             specification_generic_domain_B),
+            mins.AssociationPropertyInstance(mdf.AssociationPropertyDefinition(spec_A_2, {specification_generic_domain_B}),
+                                             mdf.OccupationStatus.occupied_known_partner,
+                                             specification_generic_domain_B)
+            ]
+
+@pytest.fixture
+def association_property_instances(spec_A_dB, spec_A_dC, spec_A_sB, spec_A_sC, spec_A_rB, spec_A_rC,
+                                   spec_A_dB_sC, spec_A_dB_sD, spec_A_dB_rC, spec_A_dB_rD, spec_A_sB_rC, spec_A_sB_rD,
+                                   specification_generic_domain_B):
+    return [assoc_instance_A(spec_A_dC, spec_A_dB, specification_generic_domain_B),
+            assoc_instance_A(spec_A_sC, spec_A_sB, specification_generic_domain_B),
+            assoc_instance_A(spec_A_rC, spec_A_rB, specification_generic_domain_B),
+            assoc_instance_A(spec_A_dB_sD, spec_A_dB_sC, specification_generic_domain_B),
+            assoc_instance_A(spec_A_dB_rD, spec_A_dB_rC, specification_generic_domain_B),
+            assoc_instance_A(spec_A_sB_rD, spec_A_sB_rC, specification_generic_domain_B)]
+
+@pytest.fixture
+def expected_association_domain_ordering(spec_A_dB, spec_A_dC, spec_A_sB, spec_A_sC, spec_A_rB, spec_A_rC,
+                                         spec_A_dB_sC, spec_A_dB_sD, spec_A_dB_rC, spec_A_dB_rD, spec_A_sB_rC, spec_A_sB_rD,
+                                         specification_generic_domain_B):
+    return [expected_ordering_assoc_instance(spec_A_dB, spec_A_dC, specification_generic_domain_B),
+            expected_ordering_assoc_instance(spec_A_sB, spec_A_sC, specification_generic_domain_B),
+            expected_ordering_assoc_instance(spec_A_rB, spec_A_rC, specification_generic_domain_B),
+            expected_ordering_assoc_instance(spec_A_dB_sC, spec_A_dB_sD, specification_generic_domain_B),
+            expected_ordering_assoc_instance(spec_A_dB_rC, spec_A_dB_rD, specification_generic_domain_B),
+            expected_ordering_assoc_instance(spec_A_sB_rC, spec_A_sB_rD, specification_generic_domain_B)]
+
+def test_association_property_instance_ordering(association_property_instances, expected_association_domain_ordering):
+    assert all(sorted(instance) == expected_association_domain_ordering[i] for i, instance in enumerate(association_property_instances) )
+
+
+# def molecule_definition_A():
+#     spec_A = spe.Specification("A", None, None, None)
+#     mol_def_A = mdf.MoleculeDefinition(
+#         spec_A,
+#         {mdf.ModificationPropertyDefinition(spec_A_psite, {mdf.Modifier.unmodified, mdf.Modifier.phosphorylated})},
+#         {mdf.AssociationPropertyDefinition(spec_A_Bassoc, {spec_B_Aassoc})},
+#         None
+#     )
+def modification_definition_A():
+    spec_A_psite = spe.Specification('A', None, None, 'psite')
+
+    spec_B_Aassoc = spe.Specification("B", 'Aassoc', None, None)
+
+    spec_A_psite = spe.Specification('A', None, None, 'psite')
+
+
+
+    assoc_inst_A_bound = mins.AssociationPropertyInstance(mdf.AssociationPropertyDefinition(spec_A_Bassoc, {spec_B_Aassoc}),
+                                                          mdf.OccupationStatus.occupied_known_partner,
+                                                          spec_B_Aassoc)
+
+    assoc_inst_A_free = mins.AssociationPropertyInstance(mdf.AssociationPropertyDefinition(spec_A_Bassoc, {spec_B_Aassoc}),
+                                                         mdf.OccupationStatus.not_occupied,
+                                                         None)
+
+    mod_inst_A_phos = mins.ModificationPropertyInstance(mdf.ModificationPropertyDefinition(spec_A_psite, {mdf.Modifier.unmodified, mdf.Modifier.phosphorylated}),
+                                                        mdf.Modifier.phosphorylated)
+
+    mod_inst_A_unphos = mins.ModificationPropertyInstance(mdf.ModificationPropertyDefinition(spec_A_psite, {mdf.Modifier.unmodified, mdf.Modifier.phosphorylated}),
+                                                          mdf.Modifier.unmodified)
