031e11e6343366f681e8c48f23dfc10766908366
thieme <sebastian.thieme@hu-berlin.de>
2016-03-22 19:41:56 +0100

    eleminated possible Emptysets or Universalsets in vennsets. wrote test for more complex example

--- rxncon/test/simulation/bBM/test_nBM_boolnet_exporter.py
+++ rxncon/test/simulation/bBM/test_nBM_boolnet_exporter.py
@@ -34,21 +34,36 @@ def test_boolnet_string(rule_A__B, rule_A_ppi_B, rule_A_p, rule_C_pplus_A, initi
 A, A
 B, B
 C, C
-A..B, ((A_ppi_B & A._p_) | (A..B & A._p_))
+A..B, (A_ppi_B | A..B)
 A_ppi_B, ((A & B) & A._p_)
 A._p_, (C_pplus_A | A._p_)
 C_pplus_A, (C & A)"""
 
     assert bbe_str == expected_str
 
+def test_boolnet_string_with_complement(rule_A__B, rule_A_ppi_B, rule_A_p_deg, rule_C_pplus_A, initialConditions):
+    bbm_system = bbm.Bipartite_Boolean_Model([rule_A__B, rule_A_ppi_B, rule_A_p_deg, rule_C_pplus_A],
+                                             initialConditions)
+
+    bbe_system = bbe.BoolNet_System(bbm_system)
+    bbe_str = bbe_system.to_string()
+    expected_str = """target, factors
+A, A
+B, B
+C, C
+A..B, (A_ppi_B | A..B)
+A_ppi_B, ((A & B) & A._p_)
+A._p_, ((C_pplus_A | A._p_) & (! D_pminus_A & ! E_pminus_A))
+C_pplus_A, (C & A)"""
+
+    assert bbe_str == expected_str
 
 @pytest.fixture
 def rule_A__B():
 
-    value_A__B = venn.Union(venn.Intersection(venn.PropertySet(bbm.Node(rfs.reaction_from_string("A_ppi_B"))),
-                               venn.PropertySet(bbm.Node(rfs.state_from_string("A-{P}")))),
-                       venn.Intersection(venn.PropertySet(bbm.Node(rfs.state_from_string("A--B"))),
-                               venn.PropertySet(bbm.Node(rfs.state_from_string("A-{P}")))))
+    value_A__B = venn.Union(venn.PropertySet(bbm.Node(rfs.reaction_from_string("A_ppi_B"))),
+                            venn.PropertySet(bbm.Node(rfs.state_from_string("A--B"))),
+                            )
 
     return bbm.Rule(bbm.Node(rfs.state_from_string("A--B")), bbm.Factor(value_A__B))
 
@@ -70,10 +85,28 @@ def rule_A_p():
 
 
 @pytest.fixture
+def rule_A_p_deg():
+    value_A_p_deg = venn.Intersection(venn.Union(venn.PropertySet(bbm.Node(rfs.reaction_from_string("C_p+_A"))),
+                                                 venn.PropertySet(bbm.Node(rfs.state_from_string("A-{P}")))),
+                                      venn.Intersection(venn.Complement(venn.PropertySet(bbm.Node(rfs.reaction_from_string("D_p-_A")))),
+                                                        venn.Complement(venn.PropertySet(bbm.Node(rfs.reaction_from_string("E_p-_A")))))
+                                      )
+
+    return bbm.Rule(bbm.Node(rfs.state_from_string("A-{P}")), bbm.Factor(value_A_p_deg))
+
+
+@pytest.fixture
 def rule_C_pplus_A():
     value_C_pplus_A = venn.Intersection(venn.PropertySet(bbm.Node(rfs.reaction_from_string("C_p+_A").components[0])),
                                         venn.PropertySet(bbm.Node(rfs.reaction_from_string("C_p+_A").components[1])))
     return bbm.Rule(bbm.Node(rfs.reaction_from_string("C_p+_A")), bbm.Factor(value_C_pplus_A))
+
+
+@pytest.fixture
+def rule_D_pminus_A():
+    value_D_pminus_A = venn.Intersection(venn.PropertySet(bbm.Node(rfs.reaction_from_string("D_p-_A").components[0])),
+                                        venn.PropertySet(bbm.Node(rfs.reaction_from_string("D_p-_A").components[1])))
+    return bbm.Rule(bbm.Node(rfs.reaction_from_string("D_p-_A")), bbm.Factor(value_D_pminus_A))
 
 
 @pytest.fixture
