9f10c444fc36d1e4db769c5b5d62104239de1788
jesperromers <jesperromers@jcr-mba.local>
2016-03-22 15:47:38 +0100

    molecule instance from string, no tests yet

--- rxncon/semantics/molecule_from_string.py
+++ rxncon/semantics/molecule_from_string.py
@@ -1,6 +1,8 @@
 from rxncon.semantics.molecule_definition import MoleculeDefinition, \
     AssociationPropertyDefinition, LocalizationPropertyDefinition, ModificationPropertyDefinition, \
-    Modifier, Compartment
+    Modifier, Compartment, OccupationStatus
+from rxncon.semantics.molecule_instance import MoleculeInstance, \
+    AssociationPropertyInstance, LocalizationPropertyInstance, ModificationPropertyInstance
 
 from rxncon.syntax.rxncon_from_string import component_from_string
 
@@ -75,3 +77,84 @@ def _loc_property_def_from_string(def_string):
         compartments.add(Compartment(compartment_string))
 
     return LocalizationPropertyDefinition(compartments)
+
+
+def mol_ins_from_string(mol_def, mol_ins_string: str) -> MoleculeInstance:
+    if isinstance(mol_def, str):
+        mol_def = mol_def_from_string(mol_def)
+
+    assert isinstance(mol_def, MoleculeDefinition)
+
+    property_instance_strings = mol_ins_string.split('#')[1].split(',')
+    property_instances = [_property_ins_from_string(mol_def, x) for x in property_instance_strings if x]
+
+    mod_props = {x for x in property_instances if isinstance(x, ModificationPropertyInstance)}
+    ass_props = {x for x in property_instances if isinstance(x, AssociationPropertyInstance)}
+    loc_props = {x for x in property_instances if isinstance(x, LocalizationPropertyInstance)}
+
+    assert len(loc_props) <= 1
+    if loc_props:
+        loc_prop = list(loc_props)[0]
+    else:
+        loc_prop = None
+
+    return MoleculeInstance(mol_def, mod_props, ass_props, loc_prop)
+
+
+def _property_ins_from_string(mol_def, prop_string):
+    identifier = prop_string[0:3]
+    if identifier == 'ass':
+        return _ass_property_ins_from_string(mol_def, prop_string)
+    elif identifier == 'mod':
+        return _mod_property_ins_from_string(mol_def, prop_string)
+    elif identifier == 'loc':
+        return _loc_property_ins_from_string(mol_def, prop_string)
+    else:
+        raise NotImplementedError
+
+
+def _ass_property_ins_from_string(mol_def, prop_string):
+    assert prop_string[0:4] == 'ass/'
+    prop_string = prop_string[4:]
+
+    assert len(prop_string.split(':')[0]) == 2
+
+    ass_domain = component_from_string(prop_string.split(':')[0])
+    ass_defs = [x for x in mol_def.association_defs if x.spec == ass_domain]
+    assert len(ass_defs) == 1
+    ass_def = ass_defs[0]
+
+    partner_domain_string = prop_string.split(':')[1]
+    if partner_domain_string:
+        return AssociationPropertyInstance(ass_def, OccupationStatus.occupied_known_partner, component_from_string(partner_domain_string))
+    else:
+        return AssociationPropertyInstance(ass_def, OccupationStatus.not_occupied, None)
+
+
+def _mod_property_ins_from_string(mol_def, prop_string):
+    assert prop_string[0:4] == 'mod/'
+    prop_string = prop_string[4:]
+
+    assert len(prop_string.split(':')[0]) == 2
+
+    mod_domain = component_from_string(prop_string.split(':')[0])
+    mod_defs = [x for x in mol_def.modification_defs if x.spec == mod_domain]
+    assert len(mod_defs) == 1
+    mod_def = mod_defs[0]
+
+    mod_string = prop_string.split(':')[1]
+    return ModificationPropertyInstance(mod_def, Modifier(mod_string))
+
+
+def _loc_property_ins_from_string(mol_def, prop_string):
+    assert prop_string[0:4] == 'mod/'
+    prop_string = prop_string[4:]
+
+    return ModificationPropertyInstance(mol_def.localization_def, Modifier(prop_string))
+
+
+
+
+
+
+
