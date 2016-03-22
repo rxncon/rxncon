479ddf56287a06dc24b6c9a799b6a8f6c0659b44
jesperromers <jesperromers@jcr-mba.local>
2016-03-22 16:41:56 +0100

    molecule instance from strings + first tests

--- rxncon/test/semantics/test_molecule_from_string.py
+++ rxncon/test/semantics/test_molecule_from_string.py
@@ -1,14 +1,23 @@
 import pytest
 
-from rxncon.semantics.molecule_from_string import mol_def_from_string
+from rxncon.semantics.molecule_from_string import mol_def_from_string, mol_ins_from_string
 from rxncon.semantics.molecule_definition import MoleculeDefinition, ModificationPropertyDefinition, \
-    AssociationPropertyDefinition, LocalizationPropertyDefinition, Compartment, Modifier
+    AssociationPropertyDefinition, LocalizationPropertyDefinition, Compartment, Modifier, OccupationStatus
+from rxncon.semantics.molecule_instance import MoleculeInstance, AssociationPropertyInstance
 from rxncon.syntax.rxncon_from_string import component_from_string
 
 
 def test_mol_def_from_string(mol_defs):
     for mol_def_string, mol_def in mol_defs.items():
         assert mol_def_from_string(mol_def_string) == mol_def
+
+
+def test_mol_ins_from_string(mol_instances):
+    for strings, mol_ins in mol_instances.items():
+        def_string = strings[0]
+        ins_string = strings[1]
+
+        assert mol_ins_from_string(def_string, ins_string) == mol_ins
 
 
 @pytest.fixture
@@ -46,3 +55,27 @@ def mol_defs():
         )
     }
 
+
+@pytest.fixture
+def mol_instances():
+    mol_instances = {}
+
+    ass_def = list(mol_def_from_string('A#ass/A_[x]:B_[y]~C_[z]').association_defs)[0]
+
+    mol_instances[('A#ass/A_[x]:B_[y]~C_[z]', 'A#ass/A_[x]:')] = MoleculeInstance(
+        mol_def_from_string('A#ass/A_[x]:B_[y]~C_[z]'),
+        set(),
+        {AssociationPropertyInstance(ass_def, OccupationStatus.not_occupied, None)},
+        None
+    )
+
+    ass_def = list(mol_def_from_string('A#ass/A_[x]:B_[y]~C_[z]').association_defs)[0]
+
+    mol_instances[('A#ass/A_[x]:B_[y]~C_[z]', 'A#ass/A_[x]:C_[z]')] = MoleculeInstance(
+        mol_def_from_string('A#ass/A_[x]:B_[y]~C_[z]'),
+        set(),
+        {AssociationPropertyInstance(ass_def, OccupationStatus.occupied_known_partner, component_from_string('C_[z]'))},
+        None
+    )
+
+    return mol_instances
