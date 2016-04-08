78de2df615dd25dbfb930028986c83f9a2575749
thieme <sebastian.thieme@hu-berlin.de>
2016-04-08 15:26:25 +0200

    sorting of mol_def seems to work

--- rxncon/test/semantics/test_molecule_definition.py
+++ rxncon/test/semantics/test_molecule_definition.py
@@ -6,7 +6,9 @@ def molecule_definitions():
     return [
         [mol_def_from_string('C#'), mol_def_from_string('A#'), mol_def_from_string('B#')],
         [mol_def_from_string('C#ass/C_[a]:A[z]'), mol_def_from_string('A#ass/A_[z]:C[a]~B_[a]'),
-         mol_def_from_string('B#ass/B_[a]:A[z]')]
+         mol_def_from_string('B#ass/B_[a]:A[z]')],
+        [mol_def_from_string('CmRNA#'), mol_def_from_string('B#'), mol_def_from_string('A#')]
+
     ]
 
 @pytest.fixture
@@ -14,7 +16,8 @@ def expected_molecule_definition_order():
     return [
         [mol_def_from_string('A#'), mol_def_from_string('B#'), mol_def_from_string('C#')],
         [mol_def_from_string('A#ass/A_[z]:C[a]~B_[a]'), mol_def_from_string('B#ass/B_[a]:A[z]'),
-         mol_def_from_string('C#ass/C_[a]:A[z]')]
+         mol_def_from_string('C#ass/C_[a]:A[z]')],
+        [mol_def_from_string('CmRNA#'), mol_def_from_string('A#'), mol_def_from_string('B#')]
     ]
 
 def test_molecule_definition_ordering(molecule_definitions, expected_molecule_definition_order):
