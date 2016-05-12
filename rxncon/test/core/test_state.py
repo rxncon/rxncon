15ab70144f3cedf5a3ea69a0bb1cbd05bd546c1b
thieme <sebastian.thieme@hu-berlin.de>
2016-05-12 14:10:51 +0200

    deleted comments

--- rxncon/test/core/test_state.py
+++ rxncon/test/core/test_state.py
@@ -6,13 +6,6 @@ from collections import namedtuple
 HierarchyTestCase = namedtuple('HierarchyTestCase', ['state', 'superspecification_of'])
 
 
-#def test_subspecification(the_case_subspecification):
-#    for test_case in the_case_subspecification:
-#        for sub_spec in test_case.subspecification_of:
-#            assert test_case.state.is_subspecification_of(sub_spec)
-#            assert not test_case.state.is_superspecification_of(sub_spec)
-
-
 def test_hierarchy(the_case_hierarchy):
     for test_case in the_case_hierarchy:
         is_hierarchy_correct(test_case)
@@ -101,52 +94,3 @@ def the_case_no_hierarchy():
         HierarchyTestCase(rfs.state_from_string('A'),
                           [rfs.state_from_string('B')])
     ]
-
-#def test_superspec_subspec_ppi():
-    # Superspecification
-    #assert rfs.state_from_string('A--B').is_superspecification_of(rfs.state_from_string('A_[n]--B'))
-    #assert rfs.state_from_string('A--B').is_superspecification_of(rfs.state_from_string('A--B_[m]'))
-    #assert rfs.state_from_string('A--B').is_superspecification_of(rfs.state_from_string('A_[n]--B_[m]'))
-
-    #assert not rfs.state_from_string('A_[n]--B').is_superspecification_of(rfs.state_from_string('A--B_[m]'))
-    #assert not rfs.state_from_string('A--B_[m]').is_superspecification_of(rfs.state_from_string('A_[n]--B'))
-    #assert rfs.state_from_string('A_[n]--B').is_superspecification_of(rfs.state_from_string('A_[n]--B_[m]'))
-    #assert rfs.state_from_string('A--B_[m]').is_superspecification_of(rfs.state_from_string('A_[n]--B_[m]'))
-
-    # Subspecification
-    #assert rfs.state_from_string('A_[n]--B').is_subspecification_of(rfs.state_from_string('A--B'))
-    #assert rfs.state_from_string('A--B_[m]').is_subspecification_of(rfs.state_from_string('A--B'))
-    #assert rfs.state_from_string('A_[n]--B_[m]').is_subspecification_of(rfs.state_from_string('A--B'))
-
-    #assert not rfs.state_from_string('A--B_[m]').is_subspecification_of(rfs.state_from_string('A_[n]--B'))
-    #assert not rfs.state_from_string('A_[n]--B').is_subspecification_of(rfs.state_from_string('A--B_[m]'))
-    #assert rfs.state_from_string('A_[n]--B_[m]').is_subspecification_of(rfs.state_from_string('A_[n]--B'))
-    #assert rfs.state_from_string('A_[n]--B_[m]').is_subspecification_of(rfs.state_from_string('A--B_[m]'))
-
-# def test_superspec_subspec_component_state():
-#     assert rfs.state_from_string('A').is_superspecification_of(rfs.state_from_string('A'))
-#
-#     assert rfs.state_from_string('A').is_superspecification_of(rfs.state_from_string('A--B'))
-#     assert rfs.state_from_string('B').is_superspecification_of(rfs.state_from_string('A--B'))
-#     assert rfs.state_from_string('A').is_superspecification_of(rfs.state_from_string('A_[d]--B'))
-#     assert rfs.state_from_string('A').is_superspecification_of(rfs.state_from_string('A_[d/s]--B'))
-#     assert rfs.state_from_string('A').is_superspecification_of(rfs.state_from_string('A_[d/s(r)]--B'))
-#
-#     assert rfs.state_from_string('A').is_superspecification_of(rfs.state_from_string('A-{p}'))
-#     assert rfs.state_from_string('A').is_superspecification_of(rfs.state_from_string('A_[d]-{p}'))
-#     assert rfs.state_from_string('A').is_superspecification_of(rfs.state_from_string('A_[d/s]-{p}'))
-#     assert rfs.state_from_string('A').is_superspecification_of(rfs.state_from_string('A_[d/s(r)]-{p}'))
-#     assert rfs.state_from_string('A').is_superspecification_of(rfs.state_from_string('A_[(r)]-{p}'))
-#
-#     assert rfs.state_from_string('A').is_subspecification_of(rfs.state_from_string('A'))
-#     assert not rfs.state_from_string('A').is_subspecification_of(rfs.state_from_string('A--B'))
-#     assert not rfs.state_from_string('B').is_subspecification_of(rfs.state_from_string('A--B'))
-#     assert not rfs.state_from_string('A').is_subspecification_of(rfs.state_from_string('A_[d]--B'))
-#     assert not rfs.state_from_string('A').is_subspecification_of(rfs.state_from_string('A_[d/s]--B'))
-#     assert not rfs.state_from_string('A').is_subspecification_of(rfs.state_from_string('A_[d/s(r)]--B'))
-#
-#     assert not rfs.state_from_string('A').is_subspecification_of(rfs.state_from_string('A-{p}'))
-#     assert not rfs.state_from_string('A').is_subspecification_of(rfs.state_from_string('A_[d]-{p}'))
-#     assert not rfs.state_from_string('A').is_subspecification_of(rfs.state_from_string('A_[d/s]-{p}'))
-#     assert not rfs.state_from_string('A').is_subspecification_of(rfs.state_from_string('A_[d/s(r)]-{p}'))
-#     assert not rfs.state_from_string('A').is_subspecification_of(rfs.state_from_string('A_[(r)]-{p}'))
