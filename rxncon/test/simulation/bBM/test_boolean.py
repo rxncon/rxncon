498d613f694f4cfd3ecc109d955c578ebc13c935
Jesper Romers <j.c.romers@gmail.com>
2016-10-07 15:35:39 +0200

    insulin fully works

--- rxncon/test/simulation/bBM/test_boolean.py
+++ rxncon/test/simulation/bBM/test_boolean.py
@@ -135,8 +135,8 @@ def test_insulin_system():
         'IR_p+_IRS_[(Y)]':                 '{0} & {1} & IR_[JMY972]--IRS_[PTB] & IR_[TK(Y1158)]-{{p}} & IR_[TK(Y1162)]-{{p}} & IR_[TK(Y1163)]-{{p}} & IR_[IRBD]--IR_[IRBD] & IR_lig]--insulin_[IR]'.format(IR, IRS),
         'Grb2_[SOS]_ppi+_SOS_[Grb2]':      '{0} & {1}'.format(Grb2, SOS),
         'Grb2_[SOS]_ppi-_SOS_[Grb2]':      '{0} & {1}'.format(Grb2, SOS),
-        'Grb2_[SOS]_ppi+_IRS_[Y]':         '{0} & {1} & IRS_[(Y)]-{{p}}'.format(Grb2, IRS),
-        'Grb2_[SOS]_ppi-_IRS_[Y]':         '{0} & {1}'.format(Grb2, IRS),
+        'Grb2_[SH2]_ppi+_IRS_[Y]':         '{0} & {1} & IRS_[(Y)]-{{p}}'.format(Grb2, IRS),
+        'Grb2_[SH2]_ppi-_IRS_[Y]':         '{0} & {1}'.format(Grb2, IRS),
         'IR_[JMY972]_ppi+_Shc_[PTB]':      '{0} & {1} & IR_[JM(Y972)]-{{p}}'.format(IR, Shc),
         'IR_[JMY972]_ppi-_Shc_[PTB]':      '{0} & {1}'.format(IR, Shc),
         'IR_p+_Shc_[(YY239240)]':          '{0} & {1} & IR_[JMY972]--Shc_[PTB] & IR_[TK(Y1158)]-{{p}} & IR_[TK(Y1162)]-{{p}} & IR_[TK(Y1163)]-{{p}} & IR_[IRBD]--IR_[IRBD] & IR_[lig]--insulin_[IR]'.format(IR, Shc),
@@ -149,7 +149,7 @@ def test_insulin_system():
         'IRS_[Y]_ppi-_PI3K_[SH2]':         '{0} & {1}'.format(IRS, PI3K),
         # State updates
         'IR_[IRBD]--0':                    '{0} & (( IR_[IRBD]_ppi-_IR_[IRBD] & IR_[IRBD]--IR_[IRBD] ) | ( IR_[IRBD]--0 & ~( IR_[IRBD]_ppi+_IR_[IRBD] & IR_[IRBD]--0 )))'.format(IR),
-        'IR_[IRBD]--IR_[IRDB]':            '{0} & (( IR_[IRBD]_ppi+_IR_[IRBD] & IR_[IRBD]--0 ) | ( IR_[IRBD]--IR_[IRBD] & ~( IR_[IRBD]_ppi-_IR_[IRBD] & IR_[IRBD]--IR_[IRBD] )))'.format(IR),
+        'IR_[IRBD]--IR_[IRBD]':            '{0} & (( IR_[IRBD]_ppi+_IR_[IRBD] & IR_[IRBD]--0 ) | ( IR_[IRBD]--IR_[IRBD] & ~( IR_[IRBD]_ppi-_IR_[IRBD] & IR_[IRBD]--IR_[IRBD] )))'.format(IR),
         'IR_[lig]--0':                     '{0} & (( IR_[lig]_i-_insulin_[IR] & IR_[lig]--insulin_[IR] ) | ( IR_[lig]--0 & ~( IR_[lig]_i+_insulin_[IR] & IR_[lig]--0 & insulin_[IR]--0 )))'.format(IR),
         'IR_[lig]--insulin_[IR]':          '{0} & {1} & (( IR_[lig]_i+_insulin_[IR] & IR_[lig]--0 & insulin_[IR]--0 ) | ( IR_[lig]--insulin_[IR] & ~( IR_[lig]_i-_insulin_[IR] & IR_[lig]--insulin_[IR] )))'.format(IR, insulin),
         'insulin_[IR]--0':                 '{0} & (( IR_[lig]_i-_insulin_[IR] & IR_[lig]--insulin_[IR] ) | ( insulin_[IR]--0 & ~( IR_[lig]_i+_insulin_[IR] & IR_[lig]--0 & insulin_[IR]--0 )))'.format(insulin),
@@ -166,11 +166,38 @@ def test_insulin_system():
         'IRS_[PTB]--0':                    '{0} & (( IR_[JMY972]_ppi-_IRS_[PTB] & IR_[JMY972]--IRS_[PTB] ) | ( IRS_[PTB]--0 & ~( IR_[JMY972]_ppi+_IRS_[PTB] & IR_[JMY972]--0 & IRS_[PTB]--0 )))'.format(IRS),
         'IR_[JMY972]--Shc_[PTB]':          '{0} & {1} & (( IR_[JMY972]_ppi+_Shc_[PTB] & IR_[JMY972]--0 & Shc_[PTB]--0 ) | ( IR_[JMY972]--Shc_[PTB] & ~( IR_[JMY972]_ppi-_Shc_[PTB] & IR_[JMY972]--Shc_[PTB] )))'.format(IR, Shc),
         'Shc_[PTB]--0':                    '{0} & (( IR_[JMY972]_ppi-_Shc_[PTB] & IR_[JMY972]--Shc_[PTB] ) | ( Shc_[PTB]--0 & ~( IR_[JMY972]_ppi+_Shc_[PTB] & IR_[JMY972]--0 & Shc_[PTB]--0 )))'.format(Shc),
-
-        # 'IRS_[PH]--0':                     ''
+        'IRS_[PH]--0':                     '{0} & (( IRS_[PH]_i-_Phospholipids_[IRS] & IRS_[PH]--Phospholipids_[IRS] ) | ( IRS_[PH]--0 & ~( IRS_[PH]_i+_Phospholipids_[IRS] & IRS_[PH]--0 & Phospholipids_[IRS]--0 )))'.format(IRS),
+        'IRS_[PH]--Phospholipids_[IRS]':   '{0} & {1} & (( IRS_[PH]_i+_Phospholipids_[IRS] & IRS_[PH]--0 & Phospholipids_[IRS]--0 ) | ( IRS_[PH]--Phospholipids_[IRS] & ~( IRS_[PH]_i-_Phospholipids_[IRS] & IRS_[PH]--Phospholipids_[IRS] )))'.format(IRS, Phospholipids),
+        'Phospholipids_[IRS]--0':          '{0} & (( IRS_[PH]_i-_Phospholipids_[IRS] & IRS_[PH]--Phospholipids_[IRS] ) | ( Phospholipids_[IRS]--0 & ~( IRS_[PH]_i+_Phospholipids_[IRS] & IRS_[PH]--0 & Phospholipids_[IRS]--0 )))'.format(Phospholipids),
+        'IRS_[(Y)]-{0}':                   '{0} & ( IRS_[(Y)]-{{0}} & ~( IR_p+_IRS_[(Y)] & IRS_[(Y)]-{{0}} ))'.format(IRS),
+        'IRS_[(Y)]-{p}':                   '{0} & ( IRS_[(Y)]-{{p}} | ( IR_p+_IRS_[(Y)] & IRS_[(Y)]-{{0}} ))'.format(IRS),
+        'Grb2_[SOS]--0':                   '{0} & (( Grb2_[SOS]_ppi-_SOS_[Grb2] & Grb2_[SOS]--SOS_[Grb2] ) | ( Grb2_[SOS]--0 & ~( Grb2_[SOS]_ppi+_SOS_[Grb2] & Grb2_[SOS]--0 & SOS_[Grb2]--0 )))'.format(Grb2),
+        'Grb2_[SOS]--SOS_[Grb2]':          '{0} & {1} & (( Grb2_[SOS]_ppi+_SOS_[Grb2] & Grb2_[SOS]--0 & SOS_[Grb2]--0 ) | ( Grb2_[SOS]--SOS_[Grb2] & ~( Grb2_[SOS]_ppi-_SOS_[Grb2] & Grb2_[SOS]--SOS_[Grb2] )))'.format(Grb2, SOS),
+        'SOS_[Grb2]--0':                   '{0} & (( Grb2_[SOS]_ppi-_SOS_[Grb2] & Grb2_[SOS]--SOS_[Grb2] ) | ( SOS_[Grb2]--0 & ~( Grb2_[SOS]_ppi+_SOS_[Grb2] & Grb2_[SOS]--0 & SOS_[Grb2]--0 )))'.format(SOS),
+        'IRS_[Y]--0':                      '{0} & (( IRS_[Y]_ppi-_PI3K_[SH2] & IRS_[Y]--PI3K_[SH2] ) | ( Grb2_[SH2]_ppi-_IRS_[Y] & Grb2_[SH2]--IRS_[Y] ) | ( IRS_[Y]--0 & ~( IRS_[Y]_ppi+_PI3K_[SH2] & IRS_[Y]--0 & PI3K_[SH2]--0 ) & ~( Grb2_[SH2]_ppi+_IRS_[Y] & IRS_[Y]--0 & Grb2_[SH2]--0 )))'.format(IRS),
+        'IRS_[Y]--PI3K_[SH2]':             '{0} & {1} & (( IRS_[Y]_ppi+_PI3K_[SH2] & IRS_[Y]--0 & PI3K_[SH2]--0 ) | ( IRS_[Y]--PI3K_[SH2] & ~( IRS_[Y]_ppi-_PI3K_[SH2] & IRS_[Y]--PI3K_[SH2] )))'.format(IRS, PI3K),
+        'PI3K_[SH2]--0':                   '{0} & (( IRS_[Y]_ppi-_PI3K_[SH2] & IRS_[Y]--PI3K_[SH2] ) | ( PI3K_[SH2]--0 & ~( IRS_[Y]_ppi+_PI3K_[SH2] & IRS_[Y]--0 & PI3K_[SH2]--0 )))'.format(PI3K),
+        'Grb2_[SH2]--IRS_[Y]':             '{0} & {1} & (( Grb2_[SH2]_ppi+_IRS_[Y] & Grb2_[SH2]--0 & IRS_[Y]--0 ) | ( Grb2_[SH2]--IRS_[Y] & ~( Grb2_[SH2]_ppi-_IRS_[Y] & Grb2_[SH2]--IRS_[Y] )))'.format(Grb2, IRS),
+        'Grb2_[SH2]--0':                   '{0} & (( Grb2_[SH2]_ppi-_IRS_[Y] & Grb2_[SH2]--IRS_[Y] ) | ( Grb2_[SH2]_ppi-_Shc_[YY] & Grb2_[SH2]--Shc_[YY] ) | ( Grb2_[SH2]_ppi-_Shc_[Y] & Grb2_[SH2]--Shc_[Y] ) | ( Grb2_[SH2]--0 & ~( Grb2_[SH2]_ppi+_IRS_[Y] & Grb2_[SH2]--0 & IRS_[Y]--0 ) & ~( Grb2_[SH2]_ppi+_Shc_[YY] & Grb2_[SH2]--0 & Shc_[YY]--0 ) & ~( Grb2_[SH2]_ppi+_Shc_[Y] & Grb2_[SH2]--0 & Shc_[Y]--0 )))'.format(Grb2),
+        'Grb2_[SH2]--Shc_[YY]':            '{0} & {1} & (( Grb2_[SH2]_ppi+_Shc_[YY] & Grb2_[SH2]--0 & Shc_[YY]--0 ) | ( Grb2_[SH2]--Shc_[YY] & ~( Grb2_[SH2]_ppi-_Shc_[YY] & Grb2_[SH2]--Shc_[YY] )))'.format(Grb2, Shc),
+        'Shc_[YY]--0':                     '{0} & (( Grb2_[SH2]_ppi-_Shc_[YY] & Grb2_[SH2]--Shc_[YY] ) | ( Shc_[YY]--0 & ~( Grb2_[SH2]_ppi+_Shc_[YY] & Grb2_[SH2]--0 & Shc_[YY]--0 )))'.format(Shc),
+        'Grb2_[SH2]--Shc_[Y]':             '{0} & {1} & (( Grb2_[SH2]_ppi+_Shc_[Y] & Grb2_[SH2]--0 & Shc_[Y]--0 ) | ( Grb2_[SH2]--Shc_[Y] & ~( Grb2_[SH2]_ppi-_Shc_[Y] & Grb2_[SH2]--Shc_[Y] )))'.format(Grb2, Shc),
+        'Shc_[Y]--0':                      '{0} & (( Grb2_[SH2]_ppi-_Shc_[Y] & Grb2_[SH2]--Shc_[Y] ) | ( Shc_[Y]--0 & ~( Grb2_[SH2]_ppi+_Shc_[Y] & Grb2_[SH2]--0 & Shc_[Y]--0 )))'.format(Shc),
+        'Shc_[(YY239240)]-{0}':            '{0} & ( Shc_[(YY239240)]-{{0}} & ~( IR_p+_Shc_[(YY239240)] & Shc_[(YY239240)]-{{0}} ))'.format(Shc),
+        'Shc_[(YY239240)]-{p}':            '{0} & ( Shc_[(YY239240)]-{{p}} | ( IR_p+_Shc_[(YY239240)] & Shc_[(YY239240)]-{{0}} ))'.format(Shc),
+        'Shc_[(Y317)]-{0}':                '{0} & ( Shc_[(Y317)]-{{0}} & ~( IR_p+_Shc_[(Y317)] & Shc_[(Y317)]-{{0}} ))'.format(Shc),
+        'Shc_[(Y317)]-{p}':                '{0} & ( Shc_[(Y317)]-{{p}} | ( IR_p+_Shc_[(Y317)] & Shc_[(Y317)]-{{0}} ))'.format(Shc),
     }
 
+    assert len(boolean_model.update_rules) == len(expected_rules)
+
     for update_rule in boolean_model.update_rules:
-        if str(update_rule.target) in expected_rules.keys():
-            print(update_rule.target)
-            assert update_rule.factor.is_equivalent_to(venn_from_str(expected_rules[str(update_rule.target)], target_from_str))
+        assert update_rule.factor.is_equivalent_to(venn_from_str(expected_rules[str(update_rule.target)], target_from_str))
+
+
+
+
+
+
+
+
