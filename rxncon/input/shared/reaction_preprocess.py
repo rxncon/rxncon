7cb545bbf6cdceb0c36a2910baa02ee020012618
Jesper Romers <j.c.romers@gmail.com>
2016-10-19 10:52:00 +0200

    bugfix i+ i- expansion

--- rxncon/input/shared/reaction_preprocess.py
+++ rxncon/input/shared/reaction_preprocess.py
@@ -6,7 +6,7 @@ from rxncon.core.reaction import BIDIRECTIONAL_VERBS
 def split_bidirectional_reaction_str(rxn_str: str) -> List[str]:
     for verb in BIDIRECTIONAL_VERBS:
         if '_{}_'.format(verb).lower() in rxn_str.lower():
-            verb = re.findall('(?i){}'.format(verb), rxn_str)[0]
+            verb = re.findall('(?i)_{}_'.format(verb), rxn_str)[0][1:-1]
 
             return [
                 rxn_str.replace('_{}_'.format(verb), '_{}+_'.format(verb)),
