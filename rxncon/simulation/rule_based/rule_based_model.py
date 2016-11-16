f9d61ed2829b0edc016e2fbd32c7a0b94e7e7c48
Jesper Romers <j.c.romers@gmail.com>
2016-11-16 11:50:00 +0100

    ComplexExprBuilder

--- rxncon/simulation/rule_based/rule_based_model.py
+++ rxncon/simulation/rule_based/rule_based_model.py
@@ -1,6 +1,7 @@
 from typing import Dict, List, Optional, Tuple
 
-from rxncon.core.state import State
+from rxncon.core.rxncon_system import RxnConSystem
+from rxncon.core.state import State, StateModifier
 from rxncon.core.spec import Spec
 
 
@@ -16,70 +17,152 @@ class MolDef:
         return self.site_defs.keys()
 
 
+class MolDefBuilder:
+    def __init__(self, name: str):
+        self.name      = name  # type: str
+        self.site_defs = {}    # type: Dict[str, List[str]]
+
+    def build(self):
+        return MolDef(self.name, self.site_defs)
+
+    def add_site(self, site: str):
+        if site not in self.site_defs:
+            self.site_defs[site] = []
+
+    def add_mod_for_site(self, site: str, mod: str):
+        self.site_defs[site] += mod
+
+
 class Mol:
-    def __init__(self, mol_def: MolDef, site_to_mod: Dict[str, str], site_to_bond: Dict[str, int]):
-        self.mol_def = mol_def
-        self.sites   = mol_def.site_defs.keys()
+    def __init__(self, spec: Spec, site_to_mod: Dict[str, str], site_to_bond: Dict[str, int]):
+        assert spec.is_component_spec
+        self.spec         = spec
         self.site_to_mod  = site_to_mod
         self.site_to_bond = site_to_bond
 
-        self._validate()
-
     @property
     def name(self):
-        return self.mol_def.name
-
-    def _validate(self):
-        for site, state in self.site_to_mod.items():
-            assert state in self.mol_def.mods_for_site(site)
+        return self.spec.component_name
 
 
 def site_name(spec: Spec) -> str:
     bad_chars = ['[', ']', '/', '(', ')']
-    spec_str = str(spec.to_non_struct_spec())
+    spec_str = str(spec.locus)
     for bad_char in bad_chars:
         spec_str = spec_str.replace(bad_char, '')
 
     return spec_str
 
 
-class Building:
+class MolBuilder:
+    def __init__(self, spec: Spec):
+        self.spec         = spec
+        self.site_to_mod  = {}  # type: Dict[str, str]
+        self.site_to_bond = {}  # type: Dict[str, int]
+
+    def build(self) -> Mol:
+        return Mol(self.spec, self.site_to_mod, self.site_to_bond)
+
+    def set_bond_index(self, spec: Spec, bond_index: int):
+        self.site_to_bond[site_name(spec)] = bond_index
+
+    def set_mod(self, spec: Spec, mod: Optional[StateModifier]):
+        self.site_to_mod[site_name(spec)] = str(mod) if mod else None
+
+
+class ComplexExprBuilder:
     def __init__(self):
+        self._mol_builders = {}  # type: Dict[Spec, MolBuilder]
         self._current_bond = 0
+        self._bonds        = []  # type: List[Tuple[Spec, Spec]]
+
+    def build(self) -> List[Complex]:
+        complexes = []
+
+        for group in self._grouped_specs():
+            complexes.append(Complex([self._mol_builders[spec].build() for spec in group]))
+
+        return complexes
 
     def add_mol(self, spec: Spec):
+        if spec.to_component_spec() not in self._mol_builders:
+            self._mol_builders[spec.to_component_spec()] = MolBuilder(spec.to_component_spec())
+
+    def set_bond(self, first: Spec, second: Spec):
+        self._bonds.append((first.to_component_spec(), second.to_component_spec()))
+        self._current_bond += 1
+        self._mol_builders[first.to_component_spec()].set_bond_index(first, self._current_bond)
+        self._mol_builders[second.to_component_spec()].set_bond_index(second, self._current_bond)
+
+    def set_mod(self, spec: Spec, mod: Optional[StateModifier]):
+        self._mol_builders[spec.to_component_spec()].set_mod(spec, mod)
+
+    def _grouped_specs(self) -> List[List[Spec]]:
+        grouped_specs = [[spec] for spec in self._mol_builders.keys()]
+
+        for bond in self._bonds:
+            for i, group in enumerate(grouped_specs):
+                if bond[0] in group:
+                    grouped_specs.pop(i)
+                    other_group = next((other_group for other_group in grouped_specs if bond[1] in other_group), [])
+                    other_group += group
+                    break
+
+        return grouped_specs
 
 
-
-STATE_TO_BUILDING_FN = {
+STATE_TO_COMPLEX_BUILDER_FN = {
     # Covalent modification state.
     '$x-{$y}': [
-        lambda state, building: building.add_mol(state['$x']),
-        lambda state, building: building.set_mod(state['$x'], str(state['$y']))
+        lambda state, builder: builder.add_mol(state['$x']),
+        lambda state, builder: builder.set_mod(state['$x'], state['$y'])
     ],
     # Interaction state.
     '$x--$y': [
-        lambda state, building: building.add_mol(state['$x']),
-        lambda state, building: building.add_mol(state['$y']),
-        lambda state, building: building.set_bond(state['$x'], state['$y'])
+        lambda state, builder: builder.add_mol(state['$x']),
+        lambda state, builder: builder.add_mol(state['$y']),
+        lambda state, builder: builder.set_bond(state['$x'], state['$y'])
     ],
     # Self-interaction state.
     '$x--[$y]': [
-        lambda state, building: building.add_mol(state['$x']),
-        lambda state, building: building.set_bond(state.specs[0], state.specs[1])
+        lambda state, builder: builder.add_mol(state['$x']),
+        lambda state, builder: builder.set_bond(state.specs[0], state.specs[1])
     ],
     # Empty binding state.
     '$x--0': [
-        lambda state, building: building.add_mol(state['$x']),
-        lambda state, building: building.set_mod(state['$x'], None)
+        lambda state, builder: builder.add_mol(state['$x']),
+        lambda state, builder: builder.set_mod(state['$x'], None)
     ]
 }
 
 
+STATE_TO_MOL_DEF_BUILDER_FN = {
+    # Covalent modification state.
+    '$x-{$y}': [
+        lambda state, builder: builder.add_site(state['$x']),
+        lambda state, builder: builder.add_mod(state['$x'], state['$y'])
+    ],
+    # Interaction state.
+    '$x--$y': [
+        lambda state, builder: builder.add_site(state['$x']) if builder.name == state['$x'].component_name \
+                               else builder.add_site(state['$y']),
+    ],
+    # Self-interaction state.
+    '$x--[$y]': [
+        lambda state, builder: builder.add_site(state.specs[0]),
+        lambda state, builder: builder.add_site(state.specs[1])
+    ],
+    # Empty binding state.
+    '$x--0': [
+        lambda state, builder: builder.add_site(state['$x'])
+    ]
+}
+
+
+
 class Complex:
-    def __init__(self, mols: List[Mol], bonds: List[Tuple[Spec, Spec]]):
+    def __init__(self, mols: List[Mol]):
         self.mols  = mols
-        self.bonds = bonds
         self._validate()
 
     def _validate(self):
@@ -114,12 +197,19 @@ class RuleBasedModel:
             parameters, observables, rules
 
 
+def rule_based_model_from_rxncon(rxncon_sys: RxnConSystem) -> RuleBasedModel:
+    def mol_defs_from_rxncon(rxncon_sys: RxnConSystem) -> Dict[Spec, MolDef]:
+        mol_defs = {}
+        for spec in rxncon_sys.components():
+            builder = MolDefBuilder(spec.component_name)
+            for state in rxncon_sys.states_for_component(spec):
+                STATE_TO_MOL_DEF_BUILDER_FN[state.repr_def](state, builder)
 
+            mol_defs[spec] = builder.build()
 
+        return mol_defs
 
-
-
-
+    mol_defs = mol_defs_from_rxncon(rxncon_sys)
 
 
 
