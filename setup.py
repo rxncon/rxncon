16d3cc6f1c51223432ab445efeb465e44cc5c68e
Jesper Romers <j.c.romers@gmail.com>
2017-02-14 11:03:03 +0100

    preparation for pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from distutils.core import setup
-from Cython.Build import cythonize
 
-setup(ext_modules=cythonize("rxncon/test/simulation/rule_based/cython_cpp/hello_world.pyx",
-                            sources=["rxncon/test/simulation/rule_based/cython_cpp/HelloWorld.cpp"],
-                            language="c++"))
+setup(
+  name='rxncon',
+  packages=['rxncon'],
+  version='2.0beta0',
+  description='The reaction-contingency framework for cellular signalling processes.',
+  author='The rxncon group @ Humboldt University Berlin',
+  author_email='jesper.romers@hu-berlin.de',
+  url='https://github.com/rxncon/rxncon',
+  download_url='https://github.com/rxncon/rxncon/tarball/0.1',
+  keywords=['sysbio', 'signalling', 'systems biology'],
+  classifiers=[],
+)
