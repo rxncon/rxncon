f384b412655015ec320d260350b6b8e1e4f0c771
Jesper Romers <j.c.romers@gmail.com>
2017-04-11 10:30:19 +0200

    Test no steady state.

--- rxncon/test/simulation/boolean/test_boolean_motifs.py
+++ rxncon/test/simulation/boolean/test_boolean_motifs.py
@@ -1,5 +1,5 @@
+import pytest
 from typing import Dict
-from rxncon.core.state import state_from_str
 from rxncon.core.reaction import reaction_from_str
 from rxncon.core.rxncon_system import RxnConSystem
 from rxncon.simulation.boolean.boolean_model import boolean_model_from_rxncon, SmoothingStrategy
@@ -7,9 +7,10 @@ from rxncon.test.simulation.boolean.utils import target_from_str
 from rxncon.venntastic.sets import UniversalSet, EmptySet
 
 
-def check_motif(motif: Dict[str, bool], in_state: Dict[str, bool], out_state: Dict[str, bool]) -> bool:
+def check_motif(motif: Dict[str, bool], in_state: Dict[str, bool], out_state: Dict[str, bool],
+                smoothing: SmoothingStrategy=SmoothingStrategy.smooth_production_sources) -> bool:
     rxncon_sys = RxnConSystem([reaction_from_str(x) for x, _ in motif.items()], [])
-    boolean_model = boolean_model_from_rxncon(rxncon_sys, SmoothingStrategy.smooth_production_sources)
+    boolean_model = boolean_model_from_rxncon(rxncon_sys, smoothing)
 
     for rxn, val in motif.items():
         if val:
@@ -27,6 +28,23 @@ def check_motif(motif: Dict[str, bool], in_state: Dict[str, bool], out_state: Di
             return False
 
     return True
+
+
+def test_no_steady_state():
+    motif = {
+        'X_syn_A': False,
+        'Y_p+_A_[(r)]': True,
+        'Z_p-_A_[(r)]': True,
+        'W_deg_A': False
+    }
+
+    with pytest.raises(AssertionError):
+        assert check_motif(motif, {'A_[(r)]-{0}': True, 'A_[(r)]-{p}': False}, {'A_[(r)]-{0}': True, 'A_[(r)]-{p}': False},
+                           SmoothingStrategy.no_smoothing)
+
+    with pytest.raises(AssertionError):
+        assert check_motif(motif, {'A_[(r)]-{0}': False, 'A_[(r)]-{p}': True}, {'A_[(r)]-{0}': False, 'A_[(r)]-{p}': True},
+                           SmoothingStrategy.no_smoothing)
 
 
 def test_mod_no_reactions():
@@ -139,9 +157,6 @@ def test_mod_deg_pplus_pminus():
     assert check_motif(motif, {'A_[(r)]-{0}': True, 'A_[(r)]-{p}': False}, {'A_[(r)]-{0}': False, 'A_[(r)]-{p}': False})
     assert check_motif(motif, {'A_[(r)]-{0}': False, 'A_[(r)]-{p}': True}, {'A_[(r)]-{0}': False, 'A_[(r)]-{p}': False})
     assert check_motif(motif, {'A_[(r)]-{0}': False, 'A_[(r)]-{p}': False}, {'A_[(r)]-{0}': False, 'A_[(r)]-{p}': False})
-
-
-#######
 
 
 def test_mod_syn():
